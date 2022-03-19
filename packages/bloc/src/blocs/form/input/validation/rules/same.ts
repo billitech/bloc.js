5fd90b3c6a0b734c6fbedb64a850c6e0e782231e
Okon Sylvester Charlie <silverprince4134u@gmail.com>
2022-03-19 14:34:36 +0100

    bug fix

--- packages/bloc/src/blocs/form/input/validation/rules/same.ts
+++ packages/bloc/src/blocs/form/input/validation/rules/same.ts
@@ -3,7 +3,7 @@ import { InputBloc } from '../../input-bloc'
 import { Rule } from '../rule'
 
 
-export const IsSame = <T>(bloc: InputBloc<T, unknown>) =>
+export const IsSame = <T>(bloc: InputBloc<T, any>) =>
   new Rule<T, string>({
     errorMessage: 'Invalid {field} value',
     validator: (value: T) => {

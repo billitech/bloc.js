0efc138b23d7f805e84af760735e5aaddd41936b
Okon Sylvester Charlie <silverprince4134u@gmail.com>
2023-05-21 10:54:16 +0100

    Added stuffs

--- packages/bloc/src/blocs/form/form-bloc.ts
+++ packages/bloc/src/blocs/form/form-bloc.ts
@@ -134,5 +134,6 @@ export abstract class FormBloc extends Bloc<FormState, FormEvent> {
   dispose() {
     super.dispose()
     this.subscriptionsContainer.dispose()
+    this.fields.forEach((field) => field.dispose())
   }
 }
